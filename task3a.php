8f8967d575eb0a774cf2cd7c38388004791e9efa
kartheekkilaru <kilarukartheek3@gmail.com>
2016-02-09 21:13:34 +0530

    task3
    copy both the files

---
+++ task3a.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head><title>task3</title>
+</head>
+<body>
+<h1>Task 3A</h1>
+<!--php doesnt support form-->
+<!--php file need the method of the form to be mentioned-->
+copy task3b.php also along with this file.
+<form action="task3b.php" method="post">
+	Name:<input type="text" name="t1">
+	Nick:<input type="text" name="t2">
+	<input type="submit" value="confirm">
+</form>
+<?php
+?>
+</body>
+</html>
---
+++ task3b.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head><title>task3b</title></head>
+<body>
+<h2>HI!!</h2>
+<br>do u like me to call u <?php echo $_POST["t1"]; ?> or <?php echo $_POST["t2"]; ?>?
+</body>
+</html>
